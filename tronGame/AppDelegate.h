0258ff5a1ab19cf80ed1b1783dca5b84df626d24
Simakov Konstantin <gigabytetheone@gmail.com>
2014-01-28 10:47:10 +0600

    Initial Commit

---
+++ tronGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tronGame
+//
+//  Created by Konstantin Simakov on 28/01/14.
+//  Copyright (c) 2014 Konstantin Simakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
